b7b9d8d2b28bdce16bf80a38de477b2d26674e79
bossanova808 <bossanova808@gmail.com>
2012-02-17 17:20:30 +1100

    V0.2 - local playback done, skin file much nicer.

--- staging/script.xsqueeze/default.py
+++ staging/script.xsqueeze/default.py
@@ -27,33 +27,43 @@ if ( __name__ == "__main__" ):
     #are we running the locally installed Squeezeslave?
     if constants.CONTROLSLAVE:
       xbmc.executebuiltin("XBMC.Notification("+ constants.__addonname__ +": Starting local Squeezeslave,Wait a moment...)")
-      #system = platform.system()
-      system = "Linux"
-      Logger.log("Starting local Squeezeslave, system is " + system)
 
-      if system=="Windows":
-        exe = [constants.BINWIN]
-      if system=="Windows":
-        exe = [constants.BINWIN]
-      elif system=="Darwin":
-        exe = [constants.BINOSX]
-      elif system=="Linux":
-        is_64bits = sys.maxsize > 2**32
+      #32 or 64 bit?
+      is_64bits = sys.maxsize > 2**32
+
+      #hack to check if openelec via settings until we work out a better way
+      if constants.ISOPENELEC:
+        Logger.log("Starting local Squeezeslave, system is openelec")
         if is_64bits:
           exe = [constants.BINLIN64]
         else:
           exe = [constants.BINLIN32]
+      #else not Openelec - what are we on then?  TODO - iOS support possible???
       else:
-        xbmc.executebuiltin("XBMC.Notification("+ constants.__addonname__ +": Couldn't determine system type for squeezeslave,Bailing out...)")
-        sys.exit()
+        system = platform.system()
+        Logger.log("Starting local Squeezeslave, system is " + system)
+        if system=="Windows":
+          exe = [constants.BINWIN]
+        elif system=="Darwin":
+          exe = [constants.BINOSX]
+        elif system=="Linux":
+          if is_64bits:
+            exe = [constants.BINLIN64]
+          else:
+            exe = [constants.BINLIN32]
+        #no idea what platform we're on..best to stop now...
+        else:
+          xbmc.executebuiltin("XBMC.Notification("+ constants.__addonname__ +": Couldn't determine system type for squeezeslave,Bailing out...)")
+          sys.exit()
 
+      #builds the list ['/path/exefile','-arg1','-arg2',...]
       args = constants.SLAVEARGS.split(" ")
       args.append(constants.SERVERIP)
       exe.extend(args)
 
-      #args = constants.SERVERIP
-      Logger.log ("Attempting to start " + str(exe))
+      Logger.log ("Attempting to start Squeezelave: " + str(exe))
       try:
+        #need this to stop windows opening a console window
         if system=="Windows":
           slaveProcess = subprocess.Popen(exe, creationflags=0x08000000, shell=False)
         else:
