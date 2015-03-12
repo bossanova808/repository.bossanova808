b255b20bf9575f69580dd8ced03e2c8a38c6d7b3
Snyder <Martin.Schneider77@web.de>
2015-03-12 21:11:42 +0100

    HDMI CEC support for XSqueeze
    This commit enables XSqueeze to turn on/off hdmi devices via Ipeng
    (remote controller app) and to control the volume of an external amp.
    You need to install the Python libcec binding:
    
    https://github.com/trainman419/python-cec

--- staging/script.xsqueeze/constants.py
+++ staging/script.xsqueeze/constants.py
@@ -127,6 +127,19 @@ SECONDS_TO_PAUSE_EXIT = int(ADDON.getSetting('exitpause'))
 SECONDS_TO_PAUSE_CONNECT = int(ADDON.getSetting('connectpause'))
 SECONDS_TO_WAIT_FOR_ARTISTSLIDESHOWEXIT = int(ADDON.getSetting('waitOnAS'))
 
+###############################################################################
+#GET HDMI CEC SETTINGS
+
+CECSUPPORT = ADDON.getSetting('cecsupport')
+TURNONAVR  = ADDON.getSetting('turnonavr')
+AVRINPUT   = ADDON.getSetting('avrinput')
+TURNOFFTV  = ADDON.getSetting('turnofftv')
+ADJUSTVOL  = ADDON.getSetting('adjustvol')
+VOLFACTOR  = ADDON.getSetting('volfactor')
+
+#variable to ensure that hdmipower commands are only send once and not all the time
+HDMIPOWER = "false"
+
 ################################################################################
 # Deal with the squeezeslave executeables...
 
