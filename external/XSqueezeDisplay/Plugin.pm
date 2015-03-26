8e4751c5321d056b24756163a1b3a0d887fdffa1
bossanova808 <bossanova808@gmail.com>
2015-03-26 14:06:27 +1100

    Time remaining is working in XSqueezeDisplay...

--- external/XSqueezeDisplay/Plugin.pm
+++ external/XSqueezeDisplay/Plugin.pm
@@ -1,4 +1,4 @@
-# Plugin.pm
+# FileViewer::Plugin.pm by mh 2005-2007
 #
 # This code is derived from code with the following copyright message:
 #
@@ -6,6 +6,10 @@
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License,
 # version 2.
+#
+# Changelog
+# 1.5 - SqueezeCenter 7.0 compatibility
+# 1.0 - initial release
 
 use strict;
 
@@ -13,33 +17,47 @@ package Plugins::XSqueezeDisplay::Plugin;
 use base qw(Slim::Plugin::Base);
 
 use vars qw($VERSION);
+use Plugins::XSqueezeDisplay::Settings;
 use Slim::Utils::Strings qw (string);
 use Slim::Utils::Prefs;
-use Plugins::XSqueezeDisplay::Settings;
+use Slim::Utils::Log;
+use Time::Seconds;
+use LWP::UserAgent;
+use JSON;
 
-$VERSION = substr(q$Revision: 1.1$, 10);
+my $ua = LWP::UserAgent->new;
 
-use constant XSQUEEZEDISPLAY_MENU => 'XSqueezeDisplay.XSqueezeDisplayMenu';
+my $log = Slim::Utils::Log->addLogCategory({
+	'category'     => 'plugin.xsqueezedisplay',
+	'defaultLevel' => 'INFO',
+	'description'  => getDisplayName(),
+});
 
 
-# A logger we will use to write plugin-specific messages. 
-my $log = Slim::Utils::Log->addLogCategory( 
- { 
-     'category'     => 'plugin.xsqueezedisplay', 
-     'defaultLevel' => 'WARN',
-     'description'  => 'PLUGIN_XSQUEEZEDISPLAY' 
- }
-);
+my $prefs = preferences('plugin.xsqueezedisplay');
 
-my $fvPrefs = preferences('plugin.XSqueezeDisplay');
-my $DEFAULT_FOLDER = "DefaultFolder";
+my ($delay, $lines, $server_endpoint, $state, $timeRemaining);
 
 sub getDisplayName { return 'PLUGIN_XSQUEEZEDISPLAY'; }
 
 sub initPlugin {
 	my $class = shift;
 	$class->SUPER::initPlugin(shift);
-	Plugins::XSqueezeDisplay::Settings->new;
+
+	$VERSION = $class->_pluginDataFor('version');
+
+	Plugins::FileViewer::Settings->new;
+
+	Slim::Buttons::Common::addSaver( 'SCREENSAVER.xsqueezedisplay',
+		getFunctions(), \&setScreenSaverMode,
+		undef, getDisplayName() );
+
+	# $delay = $prefs->get('plugin_fileviewer_updateinterval') + 1;
+	$delay = 1;
+	$server_endpoint = join('', 'http://', $prefs->get('plugin_xsqueezedisplay_kodiip'),':',$prefs->get('plugin_xsqueezedisplay_kodijsonport'), '/jsonrpc');
+	myDebug(join("","Server endpoint is ", $server_endpoint));
+	$state = "Stopped";
+	$timeRemaining = "";
 }
 
 sub setMode {
@@ -52,153 +70,211 @@ sub setMode {
 		return;
 	}
 
-	DisplayFileList($client);
-}
+	my $lines = _readFile($client);
 
-my %fileitems = ();
-
-
-sub DisplayFileList
-{
-	my $client = shift;
-
-	%fileitems = (); 
- 
-	my $folder = $fvPrefs->get('basedir');
-	my $folder = $fvPrefs->get('basedir');
-	my $folder = $fvPrefs->get('basedir');
-	my $folder = $fvPrefs->get('basedir');
-
-	if (!defined $folder) {
-		$folder = DEFAULT_FOLDER;
-	}
-
-	# find all text files within the folder. 
-	FindFiles($folder);
-
-	my @names = sort keys %fileitems;
-
-	my %params =
-	(
-		# The header (first line) to display whilst in this mode.
-		header => '{PLUGIN_FILESVIEWER} {count}',
-
-		# A reference to the list of items to display.
-		listRef => \@names,
-
-		# A unique name for this mode that won't actually get displayed anywhere.
-		modeName => FILESVIEWER_MENU,
-
-		parentMode => Slim::Buttons::Common::mode($client),
-
-		onPlay => sub {
-			my ($client, $name) = @_;
-			DisplayFile($client, $folder, $name);
-		},
-
-		# An anonymous function that is called every time the user presses the RIGHT button.
-		onRight => sub {
-			my ($client, $name) = @_;
-			DisplayFile($client, $folder, $name);
-		},
-
-		# These are all menu items and so have a right-arrow overlay
-		overlayRef => sub {
-         my $client = shift; 
-         return [ undef, $client->symbols('rightarrow') ];
-      }
+	my %params = (
+		stringHeader => 1,
+		header => 'PLUGIN_XSQUEEZEDISPLAY',
+		headerAddCount => 1,
+		listRef => $lines,
+		parentMode => Slim::Buttons::Common::mode($client),		  
 	);
 
-	Slim::Buttons::Common::pushModeLeft($client, 'INPUT.Choice', \%params);
+	Slim::Buttons::Common::pushMode($client, 'INPUT.List', \%params);
 }
 
-sub FindFiles
-{
-	my $rootdir = shift;
-	my $subdir = shift;
-
-	my $dir = (defined $subdir) ? "$rootdir/$subdir" : "$rootdir";
-
-	opendir DIR, $dir or $log->error("Couldn't open folder $dir");
-	my @files = readdir DIR;
-
-	foreach my $item (@files)
-	{
-		next if ($item =~ /^\./);
-
-		my $p = "$dir/$item";
-
-		if (-d $p)
-		{
-			my $searchdir = (defined $subdir) ? "$subdir/$item" : "$item";
-			 
-			$log->debug("search subfolder $searchdir in $rootdir");
-
-			FindFiles($rootdir, $searchdir);
-		}
-		elsif ($item =~ /(.*)(\.txt)$/)
-		{
-			my $name = $1;
-
-			$log->debug("Found $name in $item");
-
-			$fileitems{$name} = (defined $subdir) ? "$subdir/$item" : "$item";
-		}
-	}
-	closedir DIR;
-}
-
-sub DisplayFile
-{
-	my ($client, $folder, $name) = @_;
-
-	$log->info("FilesViewer: Display $name");
-
-	my $file = $folder . '/' . $fileitems{$name};
- 	my @lines = ReadFile($client, $file);
-
-	my $item = {
-		title        => $name,
-		description  => join("\n\r", @lines)
-	};
-
-	Slim::Buttons::XMLBrowser::displayItemDescription($client, $item);
-}
-
-
-sub ReadFile {
-	my ($client, $filepath) = @_;
-	
-	my @lines;
-
-	$log->info("FilesViewer: Read File $filepath");
-
-	if (open MYFILE, $filepath) {
-		@lines = <MYFILE>;
-		close MYFILE;
-	}
-	else {
-		push @lines, $client->string('PLUGIN_FILESVIEWER_NOFILE') . ": $filepath";
-	}
-
-	return @lines;
-}
-
-my %FilesViewerFunctions = (
+my %xSqueezeDisplayFunctions = (
 	'done' => sub {
 		my ( $client, $funct, $functarg ) = @_;
 		Slim::Buttons::Common::popMode($client);
 		$client->update();
 
 		# pass along ir code to new mode if requested
-		if (defined $functarg && $functarg eq 'passback') {
+		if ( defined $functarg && $functarg eq 'passback' ) {
 			Slim::Hardware::IR::resendButton($client);
 		}
 	}
 );
 
 sub getFunctions {
-	return \%FilesViewerFunctions;
+	return \%xSqueezeDisplayFunctions;
 }
 
+sub setScreenSaverMode {
+	my $client = shift;
+	$client->modeParam('modeUpdateInterval', 1);
+	$client->lines(\&screensaverXSqueezeDisplayLines);
+}
+
+sub sec2human {
+    my $secs = shift;
+    if    ($secs >= 365*24*60*60) { return sprintf '%.1fy', $secs/(365*24*60*60) }
+    elsif ($secs >=     24*60*60) { return sprintf '%.1fd', $secs/(    24*60*60) }
+    elsif ($secs >=        60*60) { return sprintf '%.1fh', $secs/(       60*60) }
+    elsif ($secs >=           60) { return sprintf '%.1fm', $secs/(          60) }
+    else                          { return sprintf '%.1fs', $secs                }
+}
+
+sub kodiJSON {
+		my $post_data = shift;
+
+		#assemble the JSON POST request
+		my $req = HTTP::Request->new(POST => $server_endpoint);
+		$req->header('content-type' => 'application/json');
+		$req->content($post_data);
+
+		#myDebug(join("", "JSON Request to: 	",$server_endpoint));
+		#myDebug(join("", "JSON Request is: 	", $post_data));
+
+		# submit the request
+		my $resp = $ua->request($req);
+
+		#  YAY! 
+		if ($resp->is_success) {
+		    #my $message = $resp->decoded_content;
+		    #myDebug("JSON Request Success: $message\n");
+		    return $resp;
+		}
+		#oh dear....
+		else {
+		    myDebug(join("JSON Request error code: 		", $resp->code, "\n"));
+		    myDebug(join("JSON Request error message: 	", $resp->message, "\n"));
+			return $resp;
+		}	
+	    
+	    
+}
+
+sub screensaverXSqueezeDisplayLines {
+
+	my $client = shift;
+
+	$lines = join(' ', @{_readFile($client)});
+
+	# initial hack in of JSON request to XBMC
+	my $req = HTTP::Request->new(POST => $server_endpoint);
+	$req->header('content-type' => 'application/json');
+
+	#debug - introspect test the kodi json API here
+	# my $post_data = '{ "jsonrpc": "2.0", "method": "JSONRPC.Introspect", "params": { "filter": { "id": "Player.GetItem", "type": "method" } }, "id": 1 }';
+
+	# Get the active players
+
+	my $post_data = '{
+		"jsonrpc": "2.0", 
+		"method": "Player.GetActivePlayers", 
+		"id": 1
+		}';
+
+	my $resp = kodiJSON($post_data);
+
+	if ($resp->is_success) {
+		my $message = decode_json $resp->decoded_content;
+		if (@{$message->{result}}){
+	    	myDebug(join("|","Player(s) Active...",@{$message->{result}},"]"));
+	    	$state = "Playing";
+
+
+			# Get the play progress time
+			my $post_data = '{
+			    "jsonrpc": "2.0",
+			    "method": "Player.GetProperties",
+			    "params": {
+			        "properties": [
+			            "percentage",
+			            "time",
+			            "totaltime"
+			        ],
+			        "playerid": 1
+			    },
+			    "id": 1
+			}';
+
+			$resp = kodiJSON($post_data);
+			$message = decode_json $resp->decoded_content;
+
+			my $duration = ($message->{'result'}{'totaltime'}{'minutes'} * 60) + $message->{'result'}{'totaltime'}{'seconds'};
+			my $elapsed = ($message->{'result'}{'time'}{'minutes'} * 60) + $message->{'result'}{'time'}{'seconds'};
+			my $difference = $duration - $elapsed;
+			my $hours = ($difference/(60*60))%24;
+			my $minutes = ($difference/60)%60;
+			my $seconds = $difference%60;
+			myDebug("Hours " . $hours . " Minutes " . $minutes . " Seconds " . $seconds);
+			$timeRemaining = "-" . join(":",$hours,$minutes,$seconds)
+
+		}
+	    else {
+			myDebug("Players NOT Active...");
+			$state = "Inactive"
+	    }
+	} 
+
+
+
+	return {
+		'line1' => $state,
+		'line2' => $timeRemaining
+	};
+}
+
+sub _readFile {
+	my $client = shift;
+	my @lines;
+
+	if (open MYFILE, $prefs->get('plugin_fileviewer_filename')) {
+		@lines = <MYFILE>;
+		close MYFILE;
+	}
+	else {
+		push @lines, 'TEST2';
+	}	
+
+	return \@lines;
+}
+
+sub myDebug {
+	my $msg = shift;
+	my $lvl = shift;
+	
+	if ($lvl eq "")
+	{
+		$lvl = "debug";
+	}
+	
+	$log->$lvl("*** XSqueezeDisplay *** $msg");
+}
+
+
+
+				# Get the data about the now playing item
+				# my $post_data = '{
+				#     "jsonrpc": "2.0",
+				#     "method": "Player.GetItem",
+				#     "params": {
+				#         "properties": [
+				#             "title",
+				#             "album",
+				#             "artist",
+				#             "season",
+				#             "episode",
+				#             "duration",
+				#             "showtitle",
+				#             "tvshowid",
+				#             "thumbnail",
+				#             "file",
+				#             "fanart",
+				#             "streamdetails",
+				#             "resume"
+				#         ],
+				#         "playerid": 1
+				#     },
+				#     "id": "VideoGetItem"
+				# }';
+
+				# my $resp = kodiJSON($post_data);
+
+
+
+
 1;
+
--- external/XSqueezeDisplay/Settings.pm
+++ external/XSqueezeDisplay/Settings.pm
@@ -1,3 +1,5 @@
+package Plugins::XSqueezeDisplay::Settings;
+
 # Settings.pm
 #
 # This code is derived from code with the following copyright message:
@@ -8,24 +10,51 @@
 # version 2.
 
 use strict;
-
-package Plugins::XSqueezeDisplay::Settings;
 use base qw(Slim::Web::Settings);
 
 use Slim::Utils::Prefs;
 
-my $prefs = preferences('plugin.XSqueezeDisplay');
+my $prefs = preferences('plugin.xsqueezedisplay');
+
+$prefs->setValidate({ 'validator' => 'int' }, 'xbmcport');
+
+sub new {
+	my $class = shift;
+
+	if (!defined $prefs->get('plugin_xsqueezedisplay_kodiip')) {
+		$prefs->set('plugin_xsqueezedisplay_kodiip', '127.0.0.1');
+	}
+	if (!defined $prefs->get('plugin_xsqueezedisplay_kodijsonport')) {
+		$prefs->set('plugin_xsqueezedisplay_kodijsonport', 80);
+	}
+	if (!defined $prefs->get('plugin_xsqueezedisplay_kodijsonuser')) {
+		$prefs->set('plugin_xsqueezedisplay_kodijsonuser', '');
+	}
+	if (!defined $prefs->get('plugin_xsqueezedisplay_kodijsonpassword')) {
+		$prefs->set('plugin_xsqueezedisplay_kodijsonpassword', '');
+	}
+	if (!defined $prefs->get('plugin_xsqueezedisplay_line1')) {
+		$prefs->set('plugin_xsqueezedisplay_line1', '');
+	}
+	if (!defined $prefs->get('plugin_xsqueezedisplay_line2')) {
+		$prefs->set('plugin_xsqueezedisplay_line2', '');
+	}
+
+	return $class->SUPER::new();
+}
 
 sub name {
-	return Slim::Web::HTTP::protectName('PLUGIN_XSQUEEZEDISPLAY');
+	return Slim::Web::HTTP::CSRF->protectName('PLUGIN_XSQUEEZEDISPLAY');
 }
 
 sub page {
-	return Slim::Web::HTTP::protectURI('plugins/XSqueezeDisplay/settings/basic.html');
+	return Slim::Web::HTTP::CSRF->protectURI('plugins/XSqueezeDisplay/settings.html');
 }
 
 sub prefs {
-	return ($prefs, qw(basedir));
+	return ($prefs, qw(plugin_xsqueezedisplay_kodiip plugin_xsqueezedisplay_kodijsonport plugin_xsqueezedisplay_kodijsonuser plugin_xsqueezedisplay_kodijsonpassword plugin_xsqueezedisplay_line1 plugin_xsqueezedisplay_line2));
 }
 
 1;
+
+__END__
