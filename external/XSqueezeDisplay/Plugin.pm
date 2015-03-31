58c38409e15478490ed47202d8fa3958b8d3cd20
bossanova808 <bossanova808@gmail.com>
2015-03-31 12:38:23 +1100

    Don't hammer kodi if it isn't there...tries to connect 3 times then goes to sleep for 10 seconds, just returning the time. Remove not needed lib/Net/Ping.pm

--- external/XSqueezeDisplay/Plugin.pm
+++ external/XSqueezeDisplay/Plugin.pm
@@ -21,6 +21,9 @@ use Net::Ping;
 use POSIX qw(strftime);
 use JSON;
 
+use constant CONNECTION_ATTEMPTS_BEFORE_SLEEP => 2;
+use constant SLEEP_PERIOD => 10;
+
 my $ua = LWP::UserAgent->new;
 
 my $log = Slim::Utils::Log->addLogCategory({
@@ -29,10 +32,10 @@ my $log = Slim::Utils::Log->addLogCategory({
 	'description'  => getDisplayName(),
 });
 
-
 my $prefs = preferences('plugin.xsqueezedisplay');
 
-my ($delay, $lines, $line1, $line2, $server_endpoint, $state, $timeRemaining);
+my ($delay, $lines, $line1, $line2, $server_endpoint, $state, $timeRemaining, $failed_connects, $retry_timer);
+
 
 # Video properties
 my ($duration,
@@ -91,6 +94,8 @@ sub initPlugin {
 	myDebug(join("","Kodi endpoint is ", $server_endpoint));
 	$state = "Stopped";
 	$timeRemaining = "";
+	$failed_connects = 0;
+	$retry_timer = SLEEP_PERIOD;
 }
 
 sub setMode {
@@ -163,7 +168,7 @@ sub kodiJSON {
 		#oh dear....
 		else {
 			#only log errors other than can't connect...
-			if ($resp->code != "500"){
+			if ($resp->code != 500){
 			    myDebug(join("JSON Request error code: 		", $resp->code, "\n"));
 			    myDebug(join("JSON Request error message: 	", $resp->message, "\n"));
 			}
@@ -183,70 +188,96 @@ sub screensaverXSqueezeDisplayLines {
 	$line2 = strftime "%I:%M %p", localtime;
 	$line2 =~ s/^0+//;
 
-	#debug - introspect test the kodi json API here
-	#my $post_data = '{ "jsonrpc": "2.0", "method": "JSONRPC.Introspect", "params": { "filter": { "id": "Player.GetItem", "type": "method" } }, "id": 1 }';
-
-	# Get the active players
-	my $post_data = '{
-		"jsonrpc": "2.0", 
-		"method": "Player.GetActivePlayers", 
-		"id": 1
-		}';
-
-	my $resp = kodiJSON($post_data);
-	
-	if ($resp->is_success) {
-		my $message = decode_json $resp->decoded_content;
-		
-		#A PLAYER IS ACTIVE
-		if (@{$message->{result}}){
-
-	    	#myDebug("Detected player activity - " . $resp->decoded_content);
-	    	$state = "Playing";
-
-	    	foreach my $player (@{$message->{result}}){
-	    		#myDebug("Player ". $player->{'playerid'} . " is type " . $player->{'type'});
-
-	    		if ($player->{'type'} == "video"){
-		    		# Get the play progress time
-					my $post_data = '{
-					    "jsonrpc": "2.0",
-					    "method": "Player.GetProperties",
-					    "params": {
-					        "properties": [
-					            "percentage",
-					            "time",
-					            "totaltime"
-					        ],
-					        "playerid": 1
-					    },
-					    "id": 1
-					}';
-
-					$resp = kodiJSON($post_data);
-					$message = decode_json $resp->decoded_content;
-
-					my $duration = ($message->{'result'}{'totaltime'}{'minutes'} * 60) + $message->{'result'}{'totaltime'}{'seconds'};
-					my $elapsed = ($message->{'result'}{'time'}{'minutes'} * 60) + $message->{'result'}{'time'}{'seconds'};
-					my $difference = $duration - $elapsed;
-					my $difference_hours = ($difference/(60*60))%24;
-					my $difference_minutes = ($difference/60)%60;
-					my $difference_seconds = $difference%60;
-					#myDebug("Hours " . $hours . " Minutes " . $minutes . " Seconds " . $seconds);
-					if ($difference_hours ne "0"){
-						$timeRemaining = sprintf("-%02d:%02d:%02d", $difference_hours, $difference_minutes, $difference_seconds)
-					}
-					else {
-						$timeRemaining = sprintf("-%02d:%02d", $difference_minutes, $difference_seconds);
-					}
-
-					$line1 = $state;
-					$line2 = $line2 . "   [" . $timeRemaining . "]";
-			    } #player == video	
-			} # foreach $player
+	#if we have more than 3 failed connections...
+	if ($failed_connects > CONNECTION_ATTEMPTS_BEFORE_SLEEP){
+		#if our retry timer is still above 0...
+		if ($retry_timer > 0) {
+			#myDebug("Sleeping for " . $retry_timer);
+			$retry_timer--;
 		}
-		#INACTIVE - DISPLAY TIME/DATE
-	} 
+		#retry timer has reached zero - reset the timer & connects counter
+		else{
+			#myDebug("Ok, trying to connect again");
+			$failed_connects = 0;
+			$retry_timer = SLEEP_PERIOD;
+		}
+	}
+	#failed_connects is <3, so try and do stuff
+	else {
+
+		#debug - introspect test the kodi json API here
+		#my $post_data = '{ "jsonrpc": "2.0", "method": "JSONRPC.Introspect", "params": { "filter": { "id": "Player.GetItem", "type": "method" } }, "id": 1 }';
+		# Get the active players
+		my $post_data = '{
+			"jsonrpc": "2.0", 
+			"method": "Player.GetActivePlayers", 
+			"id": 1
+			}';
+
+		my $resp = kodiJSON($post_data);
+		
+		if ($resp->is_success) {
+			my $message = decode_json $resp->decoded_content;
+			
+			#A PLAYER IS ACTIVE
+			if (@{$message->{result}}){
+
+		    	#myDebug("Detected player activity - " . $resp->decoded_content);
+		    	$state = "Playing";
+
+		    	foreach my $player (@{$message->{result}}){
+		    		#myDebug("Player ". $player->{'playerid'} . " is type " . $player->{'type'});
+
+		    		if ($player->{'type'} == "video"){
+			    		# Get the play progress time
+						my $post_data = '{
+						    "jsonrpc": "2.0",
+						    "method": "Player.GetProperties",
+						    "params": {
+						        "properties": [
+						            "percentage",
+						            "time",
+						            "totaltime"
+						        ],
+						        "playerid": 1
+						    },
+						    "id": 1
+						}';
+
+						$resp = kodiJSON($post_data);
+						$message = decode_json $resp->decoded_content;
+
+						my $duration = ($message->{'result'}{'totaltime'}{'minutes'} * 60) + $message->{'result'}{'totaltime'}{'seconds'};
+						my $elapsed = ($message->{'result'}{'time'}{'minutes'} * 60) + $message->{'result'}{'time'}{'seconds'};
+						my $difference = $duration - $elapsed;
+						my $difference_hours = ($difference/(60*60))%24;
+						my $difference_minutes = ($difference/60)%60;
+						my $difference_seconds = $difference%60;
+						#myDebug("Hours " . $hours . " Minutes " . $minutes . " Seconds " . $seconds);
+						if ($difference_hours ne "0"){
+							$timeRemaining = sprintf("-%02d:%02d:%02d", $difference_hours, $difference_minutes, $difference_seconds)
+						}
+						else {
+							$timeRemaining = sprintf("-%02d:%02d", $difference_minutes, $difference_seconds);
+						}
+
+						$line1 = $state;
+						$line2 = $line2 . "   [" . $timeRemaining . "]";
+				    } #player == video	
+				} # foreach $player
+			} #there was a result in the json
+		} #there was a resp->success
+		#no response from Kodi
+		else {
+			#count failed connections.  If we get to 3, stop hammering the server for a while...(See top)
+			if ($resp->code == 500){
+				#myDebug("Failed to connect for " . $failed_connects);
+				$failed_connects++;
+			}
+		}	
+
+	} #failed_connects wwas less than 3
+
 
 	#we've got here - package up the data and return it
 	my $hash = {
