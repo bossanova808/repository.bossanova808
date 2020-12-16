f56df1acfda83baa321d3dcbc07c7b0a8e69128a
bossanova808 <bossanova808@gmail.com>
2020-12-16 12:01:07 +1100

    Start tidy up and add restore

--- staging/script.ozweather-skinpatcher/resources/lib/ozweather_skinpatcher.py
+++ staging/script.ozweather-skinpatcher/resources/lib/ozweather_skinpatcher.py
@@ -3,25 +3,22 @@
 from resources.lib.common import *
 import os
 import sys
-import shutil
 import xbmc
 import xbmcvfs
 
+global dialog
 
-def run():
 
-    footprints()
-    dialog = xbmcgui.Dialog()
+class Config:
+
+    global dialog
+
     patch_seekbar = get_setting_as_bool('patchseekbar')
-
     current_skin = xbmcvfs.translatePath('special://skin')
-    log(f'special://skin us [{current_skin}]')
+
+    log(f'special://skin Is [{current_skin}]')
     log(f'CWD is {CWD}')
     log(f'Patch SeekBar is {patch_seekbar}')
-
-    if 'estuary' not in current_skin and 'confluence' not in current_skin:
-        log("ERROR - skin is not confluence or estuary")
-        dialog.notification('OzWeather Skin Patcher', 'Only Confluence and Estuary are supported', xbmcgui.NOTIFICATION_ERROR, 5000)
 
     skin = ''
     skin_xml_folder = ''
@@ -38,12 +35,12 @@ def run():
 
     xml_source_folder = os.path.join(CWD, 'resources/skin-files/', skin)
     xml_destination_folder = os.path.join(current_skin, skin_xml_folder)
-    new_myweather_xml = os.path.join(xml_source_folder, 'MyWeather.xml')
-    new_seekbar_xml = os.path.join(xml_source_folder, 'DialogSeekBar.xml')
     current_myweather_xml = os.path.join(xml_destination_folder, 'MyWeather.xml')
     current_seekbar_xml = os.path.join(xml_destination_folder, 'DialogSeekBar.xml')
     backup_myweather_xml = os.path.join(xml_destination_folder, 'MyWeather.xml.original')
     backup_seekbar_xml = os.path.join(xml_destination_folder, 'DialogSeekBar.xml.original')
+    new_myweather_xml = os.path.join(xml_source_folder, 'MyWeather.xml')
+    new_seekbar_xml = os.path.join(xml_source_folder, 'DialogSeekBar.xml')
 
     log(f'Skin XML folder is {xml_destination_folder}')
     log(f'Current MyWeather.xml is {current_myweather_xml}')
@@ -53,18 +50,22 @@ def run():
     log(f'Backup MyWeather.xml will be {backup_myweather_xml}')
     log(f'Backup DialogSeekBar.xml will be {backup_seekbar_xml}')
 
-    # First, backup the existing files...just in case...we only want to do this once for each version of the skin
-    # Otherwise we'll be over-writing our backup files if they accidentally run this when not needed
-    # (assumes when a skin is updated by Kodi, all files are replaced...?)
+
+# Backup existing skin files, and install the new ones.
+# Backup occurs only once per sklin version, it will not overwrite existing .original files.
+# This assume on a skin update that all files are replaced (?)
+def patch(config):
+
+    # Backup original files
     try:
-        if not xbmcvfs.exists(backup_myweather_xml):
+        if not xbmcvfs.exists(config.backup_myweather_xml):
             log("Backing up current MyWeather.xml to MyWeather.xml.original")
-            success = xbmcvfs.copy(current_myweather_xml, backup_myweather_xml)
-            log(f'Success: {success}')
-        if patch_seekbar and not xbmcvfs.exists(backup_seekbar_xml):
+            success = xbmcvfs.copy(config.current_myweather_xml, config.backup_myweather_xml)
+            log("...done") if success else log("...failed!")
+        if config.patch_seekbar and not xbmcvfs.exists(config.backup_seekbar_xml):
             log("Backing up current DialogSeekBar.xml to DialogSeekBar.xml.original")
-            success = xbmcvfs.copy(current_seekbar_xml, backup_seekbar_xml)
-            log(f'Success: {success}')
+            success = xbmcvfs.copy(config.current_seekbar_xml, config.backup_seekbar_xml)
+            log("...done") if success else log("...failed!")
     except:
         dialog.notification('OzWeather Skin Patcher',
                             'Error backing up current skin files - bailing out here!',
@@ -72,15 +73,15 @@ def run():
                             5000)
         sys.exit(1)
 
-    # Ok, backups done, copy our skin files over...
+    # Copy new files
     try:
-        log(f'Copying OzWeather MyWeather.xml to {xml_destination_folder}')
-        success = xbmcvfs.copy(new_myweather_xml, current_myweather_xml)
-        log(f'Success: {success}')
-        if patch_seekbar:
-            log(f'Copying OzWeather DialogSeekBar.xml to {xml_destination_folder}')
-            success = xbmcvfs.copy(new_seekbar_xml, current_seekbar_xml)
-            log(f'Success: {success}')
+        log(f'Copying OzWeather MyWeather.xml to {config.xml_destination_folder}')
+        success = xbmcvfs.copy(config.new_myweather_xml, config.current_myweather_xml)
+        log("...done") if success else log("...failed!")
+        if config.patch_seekbar:
+            log(f'Copying OzWeather DialogSeekBar.xml to {config.xml_destination_folder}')
+            success = xbmcvfs.copy(config.new_seekbar_xml, config.current_seekbar_xml)
+            log("...done") if success else log("...failed!")
         else:
             log('Not copying DialogSeekBar.xml, as per addon settings')
     except:
@@ -90,15 +91,61 @@ def run():
                             5000)
         sys.exit(1)
 
-    # If we got here, all should be well
-    dialog.notification('OzWeather Skin Patcher', 'Reloading skin after successful patch',
-                        xbmcgui.NOTIFICATION_INFO, 5000)
 
-    # Reload the skin so we can enjoy the new pretty
-    xbmc.executebuiltin('ReloadSkin()')
+# Attempt to restore .original files
+def restore(config):
+    pass
 
+
+def cancel():
+    pass
+
+
+# This is 'main'...
+def run():
+
+    global dialog
+
+    footprints()
+    config = Config()
+    dialog = xbmcgui.Dialog()
+
+    # Basic sanity checking - are they running the right skin?
+    if 'estuary' not in config.current_skin and 'confluence' not in config.current_skin:
+        log("ERROR - current skin is not confluence or estuary!")
+        dialog.notification('OzWeather Skin Patcher', 'Only Confluence and Estuary are supported',
+                            xbmcgui.NOTIFICATION_ERROR, 5000)
+        sys.exit(1)
+
+    # Display initial information
+    dialog.textviewer('OzWeather Skin Patcher',
+                      """ 
+This utility will patch skin files for OzWeather radar support.\n
+Only patches the currently selected skin, and only supports Estuary and Confluence.
+Backups of the original files are saved as .original files in the skin folder
+(& can be restored by thus utility).
+
+By default patches only MyWeather.xml, but you can also have it patch DialogSeekBar.xml
+to display radar and basic weather info when media is paused.
+(must be enabled in the addon settings))
+""")
+
+    # Now confirm if the want to proceed
+    result = dialog.select('OzWeather Skin Patcher', ['Patch', 'Restore', 'Cancel'])
+    log(f'Result is {result}')
+
+    if result == 1:
+        patch(config)
+    elif result == 2:
+        restore(config)
+    else:
+        log("User cancelled operation.")
+
+    # If we got here, all should be well - reload the skin to get the new pretty
+    if result == 1 or result == 2:
+        dialog.notification('OzWeather Skin Patcher', 'Reloading skin after successful modification',
+                            xbmcgui.NOTIFICATION_INFO, 5000)
+        xbmc.executebuiltin('ReloadSkin()')
+
+    # and, we're done..
     footprints(False)
-
-
-
-
