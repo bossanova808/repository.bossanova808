c4157b3b1c6c728c4617a1f0373498455fc5180a
bossanova808 <bossanova808@gmail.com>
2021-03-08 09:42:39 +1100

    Skin Patcher v0.0.6 - better logging and error notifications

--- staging/script.ozweather-skinpatcher/resources/lib/ozweather_skinpatcher.py
+++ staging/script.ozweather-skinpatcher/resources/lib/ozweather_skinpatcher.py
@@ -57,6 +57,19 @@ class Config:
     log(f'Backup VideoFullScreen.xml will be {backup_videofullscreen_xml}')
 
 
+# Display a notification from OzWeather Skin Patcher to the Kodi user
+# By default, in this case, it's an error notification
+
+def notify(message, notification_type=xbmcgui.NOTIFICATION_ERROR, duration=5000):
+
+    global dialog
+
+    dialog.notification('OzWeather Skin Patcher',
+                        message,
+                        notification_type,
+                        duration)
+
+
 # Backup existing skin files, and install the new ones.
 # Backup occurs only once per skin version, it will not overwrite existing .original files
 # as we don't want to clobber the original backup files if they run this twice for whatever reason...
@@ -68,58 +81,88 @@ def patch(config):
         if not xbmcvfs.exists(config.backup_myweather_xml):
             log("Backing up current MyWeather.xml to MyWeather.xml.original")
             success = xbmcvfs.copy(config.current_myweather_xml, config.backup_myweather_xml)
-            log("...done") if success else log("...failed!")
+            if success:
+                log("...done")
+            else:
+                log("...failed!  Is the skin folder writeable?")
+                notify('Exiting - as error when backing up current MyWeather.xml - is skin folder writeable?')
+                sys.exit(1)
+
         if config.patch_videofullscreen and not xbmcvfs.exists(config.backup_videofullscreen_xml):
             log("Backing up current VideoFullScreen.xml to VideoFullScreen.xml.original")
             success = xbmcvfs.copy(config.current_videofullscreen_xml, config.backup_videofullscreen_xml)
-            log("...done") if success else log("...failed!")
-    except:
-        dialog.notification('OzWeather Skin Patcher',
-                            'Exception when backing up current skin files - check logs!',
-                            xbmcgui.NOTIFICATION_ERROR,
-                            5000)
+            if success:
+                log("...done")
+            else:
+                log("...failed!  Is the skin folder writeable?")
+                notify('Exiting - as error when backing up current VideoFullScreen.xml - is skin folder writeable?')
+                sys.exit(1)
+
+    except Exception as inst:
+        log(inst)
+        notify('Exception when backing up current skin files - check logs!')
         sys.exit(1)
 
     # Copy new files
     try:
         log(f'Copying OzWeather MyWeather.xml to {config.xml_destination_folder}')
         success = xbmcvfs.copy(config.new_myweather_xml, config.current_myweather_xml)
-        log("...done") if success else log("...failed!")
+        if success:
+            log("...done")
+        else:
+            log("...failed!  Is the skin folder writeable?")
+            notify('Exiting - as error when copying OzWeather MyWeather.xml - is skin folder writeable?')
+            sys.exit(1)
+
         if config.patch_videofullscreen and 'confluence' in config.current_skin:
             log(f'Copying OzWeather VideoFullScreen.xml to {config.xml_destination_folder}')
             success = xbmcvfs.copy(config.new_videofullscreen_xml, config.current_videofullscreen_xml)
-            log("...done") if success else log("...failed!")
+            if success:
+                log("...done")
+            else:
+                log("...failed!  Is the skin folder writeable?")
+                notify('Exiting - as error when copying OzWeather VideoFullScreen.xml - is skin folder writeable?')
+                sys.exit(1)
         else:
             log('Not copying VideoFullScreen.xml, as per addon settings')
-    except:
-        dialog.notification('OzWeather Skin Patcher',
-                            'Exception when copying OzWeather skin files - check logs!',
-                            xbmcgui.NOTIFICATION_ERROR,
-                            5000)
+
+    except Exception as inst:
+        log(inst)
+        notify('Exception when copying OzWeather skin files - check logs!')
         sys.exit(1)
 
 
 # Attempt to restore .original files - we jsut try and restore both, no matter what the setting is
 def restore(config):
+
     try:
         log("Restoring .original skin files")
         if xbmcvfs.exists(config.backup_myweather_xml):
             log("Copying back MyWeather.xml from MyWeather.xml.original file")
             success = xbmcvfs.copy(config.backup_myweather_xml, config.current_myweather_xml)
-            log("...done") if success else log("...failed!")
+            if success:
+                log("...done")
+            else:
+                log("...failed!  Is file present? Is the skin folder writeable?")
+                notify('Exiting - failed to restore MyWeather.xml.original - is file present?')
+                sys.exit(1)
         else:
             log("Could not find MyWeather.xml.original file, did not restore")
         if xbmcvfs.exists(config.backup_videofullscreen_xml):
             log("Copying back VideoFullScreen.xml from VideoFullScreen.xml.original file")
             success = xbmcvfs.copy(config.backup_videofullscreen_xml, config.current_videofullscreen_xml)
-            log("...done") if success else log("...failed!")
+            if success:
+                log("...done")
+            else:
+                log("...failed!  Is file present? Is the skin folder writeable?")
+                notify('Exiting - failed to restore VideoFullScreen.xml.original - is file present?')
+                sys.exit(1)
         else:
             log('Could not find VideoFullScreen.xml.original file, did not restore')
-    except:
-        dialog.notification('OzWeather Skin Patcher',
-                            'Exception when restoring skin files - check logs!',
-                            xbmcgui.NOTIFICATION_ERROR,
-                            5000)
+
+    except Exception as inst:
+        log(inst)
+        notify('Exception when restoring skin files - check logs!')
         sys.exit(1)
 
 
@@ -137,21 +180,24 @@ def run():
             and 'estouchy' not in config.current_skin \
             and 'confluence' not in config.current_skin:
         log("ERROR - current skin is not a supported skin!")
-        dialog.notification('OzWeather Skin Patcher', 'Only Estuary, Estouchy and Confluence are supported',
-                            xbmcgui.NOTIFICATION_ERROR, 5000)
+        notify('Only Estuary, Estouchy and Confluence are supported, sorry!')
         sys.exit(1)
 
     # Display initial information
     dialog.textviewer('OzWeather Skin Patcher',
                       """ 
+<Close this window to proceed to the actual patching stage> 
+
 This utility will patch skin files for OzWeather radar support.\n
 Only patches the currently selected skin, and only if that skin is Estuary, Estouchy or Confluence.
+(Or mods of those skins that still include 'estuary','estouchy', or 'confluence' in the add-in id).
+
 Backups of the original files are saved as .original files in the skin folder
 (& can be also be restored by this utility).
 
 Confluence only - by default patches only MyWeather.xml, but you can also patch VideoFullScreen.xml
 to display radar and basic weather info when media info is displayed during playback.
-(enable this in the addon settings if you wish)
+(enable this in the add-on settings if you wish)
 """)
 
     # Now confirm if they actually want to proceed
@@ -167,11 +213,11 @@ to display radar and basic weather info when media info is displayed during play
     else:
         log("User cancelled operation.")
 
-    # If we got here, all should be well - reload the skin to get the new pretty
+    # If we got here, all _should_ be well - reload the skin to get the new pretty
     if mode == 0 or mode == 1:
         log("Reloading skin to pick up changes")
         xbmc.executebuiltin('ReloadSkin()')
-        dialog.notification('OzWeather Skin Patcher', 'Action Complete (& skin reloaded)',
-                            xbmcgui.NOTIFICATION_INFO, 5000)
+        notify('Successful patch - skin reloaded.', xbmcgui.NOTIFICATION_INFO)
+
     # and, we're done..
     footprints(False)
