b563c4e9d6ff1a6f8e26c80e0f505f2d93a6ab67
bossanova808 <bossanova808@gmail.com>
2025-10-03 16:16:34 +1000

    Revert "(WIP) v0.0.1 of Jellyfin Fixer addon"
    This reverts commit 55bb541b49309ae942fa46a0ee7bd0bd2fd2f226.

--- staging/script.jellyfin-fixer/default.py
+++
@@ -1,6 +0,0 @@
-from bossanova808 import exception_logger
-from resources.lib import jellyfin_fixer
-
-if __name__ == "__main__":
-    with exception_logger.log_exception():
-        jellyfin_fixer.run()
